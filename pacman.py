b3350f400b3a3cca5fb15112da42966458f70bd9
Noah <noah1139@study.sde.dk>
2022-11-03 11:13:51 +0100

    sasasas

--- pacman.py
+++ pacman.py
@@ -3,6 +3,12 @@ import time
 import random
 import pygame as pg
 
+#musicussy
+pg.mixer.pre_init(44100, 32, 2, 1024)
+pg.mixer.init()
+pg.mixer.music.load("faith.wav")
+pg.mixer.music.play(loops = 1)
+
 ## Load images ##
 pacman_images = []
 for i in range(6):
@@ -29,9 +35,29 @@ class PacMan:
             self.y += 5
 
     def draw(self,screen):
-        pg.draw.circle(screen, (220,220,0), 
-                       (self.x,self.y), 16)
+        #pg.draw.circle(screen, (220,220,0), 
+                       #(self.x,self.y), 16)
+        screen.blit(pacman_images[3], (self.x, self.y))
+    
+class Ghost:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+        self.images = []
+        for i in range(2):
+            img = pg.image.load(f"images/ghost_{i}.png")
+            img = pg.transform.scale(img, (32,32))
+            self.images.append(img)
 
+    def move(self):
+        self.x += random.randint(-5,5)
+        self.y += random.randint(-5,5)
+    def draw(self,screen):
+        #pg.draw.circle(screen,(220,0,0),
+        #               (self.x,self.y), 16)
+        screen.blit(self.images[0], (self.x, self.y))
+
+    
 ## Screen setup ##
 pg.init()
 screen = pg.display.set_mode((600,800))
@@ -41,6 +67,12 @@ pg.display.set_icon(pacman_images[1])
 # Creating a PacMan object
 pacman = PacMan(100,100)
 #pacman2 = PacMan(200,200)
+ghosts = []
+for i in range(10):
+    ghost = Ghost(200,200)
+    ghosts.append(ghost)
+
+
 
 direction = None
 running = True
@@ -66,18 +98,25 @@ while running:
     
     # Logic
     pacman.move(direction)
+    for ghost in ghosts:
+        ghost.move()
     #pacman2.move()
 
-    screen.fill("black")
+    
 
     # Draw
+    screen.fill("brown")
     pacman.draw(screen)
+    for ghost in ghosts:
+        ghost.draw(screen)
+
     #pacman2.draw(screen)
     print("pacman.x", pacman.x)
     print("pacman.y", pacman.y)
     #print("pacman2.x", pacman2.x)
     #print("pacman2.y", pacman2.y)
     
+
     # Update screen
     pg.display.flip()
 
